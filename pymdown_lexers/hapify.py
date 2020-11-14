746571b0b40082bf98801a2b9a221c06e67f1df8
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-11-14 14:16:44 -0500

    feat: handle inline

--- pymdown_lexers/hapify.py
+++ pymdown_lexers/hapify.py
@@ -18,17 +18,17 @@ class HapifyLexer(RegexLexer):
             (r'<<<([^>]+)>>>', token.Comment),  # Evaluate
             (r'<<#([^>]+)>>', token.Comment),  # Comments
             (r'<<=([^>]+)>>', token.String),  # Interpolation
-            (r'<<(\?\?|\?|@|if|elseif|for)', token.Operator, 'condition'),  # Condition opening
             (r'<<(\@|\?\?|\?|endif|else|endfor)>>', token.Operator),  # Condition closing
+            (r'<<(\?\?|\?|@|if|elseif|for)', token.Operator, 'condition'),  # Condition opening
             (r'<<([a-zA-Z_.]+)\s+([a-zA-Z_-]+)>>', token.String),  # Name interpolation
             (r' |\t', token.Whitespace),
             (r'.', token.Text)
         ],
         'condition': [
             (r'(\d)+', token.Number),  # Operator number
+            (r'>>', token.Operator),  # Operator closing
             (r'(\/|\*|\+|\-|\bor\b|\band\b|\bnot\b|\borNot\b|\bandNot\b)', token.Keyword),  # Condition keywords
             (r'(\(|\)|\.)', token.Punctuation),  # Common syntax
-            (r'>>', token.Operator),  # Operator closing
             (r' |\t', token.Whitespace),
             (r'.', token.Name)
         ]
