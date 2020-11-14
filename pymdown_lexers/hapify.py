f9bd511d426aaa9ef73ef7d29a83fa672d8cf9fb
Edouard Demotes-Mainard <e.demotes@gmail.com>
2020-11-14 09:01:32 -0500

    fix: fix highlighting

--- pymdown_lexers/hapify.py
+++ pymdown_lexers/hapify.py
@@ -17,10 +17,10 @@ class HapifyLexer(RegexLexer):
         'root': [
             (r'<<(\?|if|else|elseif|endif)([^>]*?)>>', token.Keyword),  # Condition
             (r'<<(\@|for|endfor)([^>]*?)>>', token.Keyword),  # Iteration
-            (r'<<<(.*)>>>', token.Escape),  # Evaluate
-            (r'<<#(.*)>>', token.Comment),  # Comments
-            (r'<<=(.*)>>', token.String),  # Interpolation
-            (r'<<([a-zA-Z_.]+)\s+([a-zA-Z_]+)>>', token.String),  # Name interpolation
+            (r'<<<([^>]+)>>>', token.Comment),  # Evaluate
+            (r'<<#([^>]+)>>', token.Comment),  # Comments
+            (r'<<=([^>]+)>>', token.String),  # Interpolation
+            (r'<<([a-zA-Z_.]+)\s+([a-zA-Z_-]+)>>', token.String),  # Name interpolation
             (r' |\t', token.Whitespace),
             (r'.', token.Text)
         ]
